9aefa18ba53d374131ac88c434d130df0e017e97
litosha <maxgermax2003@gmail.com>
2020-11-17 13:17:00 +0300

    lmao

---
+++ client/js/copyright.js
@@ -0,0 +1,6 @@
+let d = new Date();
+let year = d.getFullYear();
+let flag_stack = false
+let flag_projects = false
+
+document.querySelector(".copy").innerHTML = '&copy; Copyright ' + year + ', Maksim Litovchenko'
